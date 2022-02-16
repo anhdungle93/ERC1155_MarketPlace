9a3fc87f1919aba8bceabf9fbb9c8701219f3c25
anhdungle <anhdungle634@gmail.com>
2022-02-16 11:27:12 -0500

    add function documentation

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -16,6 +16,7 @@ contract MarketPlace is ERC1155Holder {
     }
     
     // from saleID to SaleInfo
+    // the saleID is of the form keccak256(abi.encode(msg.sender, _tokenAddress, _id))
     mapping (bytes32 => SaleInfo) public inventory;
 
     event ListItem(
@@ -41,7 +42,10 @@ contract MarketPlace is ERC1155Holder {
         uint256 amount,
         uint256 price
     );
-
+    /// @param _tokenAddress the address of ERC1155
+    /// @param _id the token id inside ERC1155
+    /// @param _amount amount to list 
+    /// @param _price price of each unit in Wei
     function listItem(address _tokenAddress, uint256 _id, uint256 _amount, uint256 _price) public {
         require(_amount != 0, "MarketPlace: nothing to list.");
         bytes32 saleID = keccak256(abi.encode(msg.sender, _tokenAddress, _id));
@@ -53,6 +57,9 @@ contract MarketPlace is ERC1155Holder {
         emit ListItem(msg.sender, _tokenAddress, _id, _amount, _price);
     }
 
+    /// @param _tokenAddress the address of ERC1155
+    /// @param _id the token id inside ERC1155
+    /// @param _amount amount to remove
     function removeItem(address _tokenAddress, uint256 _id, uint256 _amount) public {
         require(_amount != 0, "MarketPlace: nothing to remove.");
         bytes32 saleID = keccak256(abi.encode(msg.sender, _tokenAddress, _id));
@@ -63,6 +70,10 @@ contract MarketPlace is ERC1155Holder {
         emit RemoveItem(msg.sender, _tokenAddress, _id, _amount);
     }
 
+    /// @param _seller the seller who listed the item
+    /// @param _tokenAddress the address of ERC1155
+    /// @param _id the token id inside ERC1155
+    /// @param _amount amount to buy
     function buyItem(address payable _seller, address _tokenAddress, uint256 _id, uint256 _amount) public payable {
         require(_amount != 0, "MarketPlace: nothing to buy.");
         bytes32 saleID = keccak256(abi.encode(_seller, _tokenAddress, _id));
@@ -77,6 +88,10 @@ contract MarketPlace is ERC1155Holder {
         emit BuyItem(msg.sender, _seller, _tokenAddress, _id, _amount, _price);
     }
 
+    /// @param _seller the seller who listed the item
+    /// @param _tokenAddress the address of ERC1155
+    /// @param _id the token id inside ERC1155
+    /// @return amount and price of each unit
     function checkItem(address _seller, address _tokenAddress, uint256 _id) public view returns(uint256 _amount, uint256 _price){
         bytes32 saleID = keccak256(abi.encode(_seller, _tokenAddress, _id));
         SaleInfo storage saleInfo = inventory[saleID];
